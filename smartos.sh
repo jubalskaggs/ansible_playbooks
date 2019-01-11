281fd7bce97b708ba6496a9dbb60054daf70a316
Jubal Skaggs <jubal@vcontext.net>
2019-01-11 10:02:50 +0000

    hack

--- smartos.sh
+++ smartos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-pkgin up -fu
-pkgin -y install ansible
+/opt/local/bin/pkgin up -fu
+/opt/local/bin/pkgin -y install ansible
 wget --no-check-certificate https://raw.githubusercontent.com/jubalskaggs/ansible_playbooks/master/ansible.cfg
 wget --no-check-certificate https://raw.githubusercontent.com/jubalskaggs/ansible_playbooks/master/provision.yml
 wget --no-check-certificate https://raw.githubusercontent.com/jubalskaggs/ansible_playbooks/master/hosts
