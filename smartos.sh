45c69ec94d2615a6a622dd7dc090b84ca6db5c5c
Jubal Skaggs <jubal@vcontext.net>
2019-01-11 09:55:27 +0000

    typo

--- smartos.sh
+++ smartos.sh
@@ -2,6 +2,6 @@
 pkgin up -fu
 pkgin -y install ansible
 wget --no-check-certificate https://raw.githubusercontent.com/jubalskaggs/ansible_playbooks/master/ansible.cfg
-wget --no-check-certificate https://raw.githubusercontent.com/jubalskaggs/ansible_playbooks/master/provison.yml
+wget --no-check-certificate https://raw.githubusercontent.com/jubalskaggs/ansible_playbooks/master/provision.yml
 wget --no-check-certificate https://raw.githubusercontent.com/jubalskaggs/ansible_playbooks/master/hosts
 ansible-playbook ./provision.yml
