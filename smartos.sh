f6f6820d95d6d49dabbc48109c2c20cc237e6d51
Jubal Skaggs <jubal@vcontext.net>
2019-01-11 10:13:21 +0000

    hack

--- smartos.sh
+++ smartos.sh
@@ -4,4 +4,4 @@
 wget --no-check-certificate https://raw.githubusercontent.com/jubalskaggs/ansible_playbooks/master/ansible.cfg
 wget --no-check-certificate https://raw.githubusercontent.com/jubalskaggs/ansible_playbooks/master/provision.yml
 wget --no-check-certificate https://raw.githubusercontent.com/jubalskaggs/ansible_playbooks/master/hosts
-ansible-playbook ./provision.yml
+/opt/local/bin/ansible-playbook ./provision.yml
